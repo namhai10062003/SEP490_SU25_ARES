49ea15ec91c231b79ad87af50fb6a9662895c6f2
namhai10062003 <emyeuanhlam15@gmail.com>
2025-05-24 14:27:26 +0700

    update cover code

--- backend/models/User.js
+++ backend/models/User.js
@@ -6,7 +6,7 @@ const userSchema = new mongoose.Schema({
     password: { type: String, required: true },
     role: {
         type: String,
-        enum: ["admin", "customer", "host"],
+        enum: ["admin", "customer", "staff"],
         default: "customer", // Mặc định là "customer" nếu không nhập
     },
     profileImage: { type: String },
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,9 @@
 import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
+// index.js hoặc App.js
+import '@fortawesome/fontawesome-free/css/all.min.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 import { default as AuthProvider, default as useAuth } from "../context/authContext";
 
