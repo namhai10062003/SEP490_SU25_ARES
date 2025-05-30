47f027719c5713d1c0999fb0b0d7e1766fe64e65
namhai10062003 <emyeuanhlam15@gmail.com>
2025-05-30 22:24:37 +0700

    Revert "login google"

--- backend/router/auth.js
+++ backend/router/auth.js
@@ -1,22 +1,13 @@
 import express from 'express';
-import {
-  forgotPassword,
-  login,
-  register,
-  resetPassword,
-  verify,
-  verifynormal,
-  googleAuth,
-} from '../controllers/authController.js';
+import { forgotPassword, login, register, resetPassword, verify, verifynormal } from '../controllers/authController.js';
 import verifyUser from '../middleware/authMiddleware.js';
-const router = express.Router();
+const router = express.Router()
 
-router.post('/google', googleAuth);
-router.post('/login', login);
-router.post('/verify-otp', verify);
-router.post('/verify', verifyUser, verifynormal);
-router.post('/register', register);
-router.post('/forgot-password', forgotPassword);
-router.post('/reset-password', resetPassword);
-
+router.post('/login', login)
+router.post("/verify-otp", verify);
+router.post('/verify', verifyUser, verifynormal)
+router.post('/register', register)
+router.post("/verify-otp", verify); 
+router.post("/forgot-password", forgotPassword);
+router.post("/reset-password", resetPassword);
 export default router;
