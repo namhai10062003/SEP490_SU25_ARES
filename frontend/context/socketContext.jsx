ba23353048aa80ff9c7e9c2b90f4ed66d782c707
tinntse62275 <tinntse62275@fpt.edu.vn>
2025-06-17 17:29:39 +0700

    add notification when blocked user account

---
+++ frontend/context/socketContext.jsx
@@ -0,0 +1,47 @@
+import PropTypes from "prop-types";
+import React, { createContext, useContext, useEffect, useRef, useState } from "react";
+import { io } from "socket.io-client";
+import { useAuth } from "./authContext";
+
+// Tạo context
+const SocketContext = createContext();
+
+// Custom hook
+export const useSocket = () => useContext(SocketContext);
+
+// Provider
+const SocketProvider = ({ children }) => {
+    const { user } = useAuth();
+    const [socketConnected, setSocketConnected] = useState(false);
+    const socketRef = useRef(null);
+
+    useEffect(() => {
+        if (user?._id) {
+            socketRef.current = io("http://localhost:4000");
+            socketRef.current.emit("register", user._id);
+            setSocketConnected(true);
+        } else {
+            setSocketConnected(false);
+        }
+
+        return () => {
+            if (socketRef.current) {
+                socketRef.current.disconnect();
+                socketRef.current = null;
+            }
+            setSocketConnected(false);
+        };
+    }, [user?._id]);
+
+    return (
+        <SocketContext.Provider value={{ socket: socketRef.current, socketConnected }}>
+            {children}
+        </SocketContext.Provider>
+    );
+};
+
+SocketProvider.propTypes = {
+    children: PropTypes.node.isRequired,
+};
+
+export default SocketProvider;
