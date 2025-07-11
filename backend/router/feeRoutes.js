13e820fa11b1e0b4508effebd78d1bfc081b8082
namhai10062003 <emyeuanhlam15@gmail.com>
2025-07-11 13:12:12 +0700

    fix payment fee user

--- backend/router/feeRoutes.js
+++ backend/router/feeRoutes.js
@@ -1,9 +1,15 @@
 import express from "express";
-import { calculateAndSaveFees, getAllFees } from "../controllers/feeController.js";
-
+import { calculateAndSaveFees, getAllFees, getFeeByApartmentAndMonth, getMonthlyFeeByApartment, updateParkingFee } from "../controllers/feeController.js";
+import { createFeePayment, handleFeePaymentWebhook } from "../controllers/feepaymentController.js";
 const router = express.Router();
 
 router.post("/calculate", calculateAndSaveFees); // gọi tính toán & lưu
 router.get("/", getAllFees); // lấy danh sách đã lưu
+router.get("/monthly/:apartmentId", getMonthlyFeeByApartment);
+router.get("/detail/:apartmentId/:month", getFeeByApartmentAndMonth);// hàm lấy tiền theo tháng
+router.patch('/update-parking-fee/:apartmentId/:month', updateParkingFee);//hàm update tiền bãi đỗ xe
+// thanh toán chi phí hóa đơn
 
+router.post("/pay", createFeePayment);
+router.post("/webhook", handleFeePaymentWebhook);
 export default router;
---
+++ frontend/src/service/feePayment.js
@@ -0,0 +1,9 @@
+// src/api/payment.js
+import axios from "axios";
+
+export const createFeePayment = async (apartmentId, month) => {
+  return axios.post(`${import.meta.env.VITE_API_URL}/api/fees/pay`, {
+    apartmentId,
+    month,
+  });
+};
