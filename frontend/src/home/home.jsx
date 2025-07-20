0b23a2f067c355b57860ea93a14d34b61de4adb5
namhai10062003 <emyeuanhlam15@gmail.com>
2025-07-20 13:55:16 +0700

    fix again home vs post

--- frontend/src/home/home.jsx
+++ frontend/src/home/home.jsx
@@ -417,26 +417,12 @@ const Home = () => {
 
         {/* PAGINATION */}
         <div className="d-flex justify-content-center mt-4">
-          <nav>
-            <ul className="pagination">
-              <li className={`page-item ${currentPage === 1 ? "disabled" : ""}`}>
-                <button className="page-link" onClick={() => handlePageChange(currentPage - 1)}>Trước</button>
-              </li>
-              {[...Array(totalPages)].map((_, index) => (
-                <li
-                  key={index}
-                  className={`page-item ${currentPage === index + 1 ? "active" : ""}`}
-                >
-                  <button className="page-link" onClick={() => handlePageChange(index + 1)}>
-                    {index + 1}
-                  </button>
-                </li>
-              ))}
-              <li className={`page-item ${currentPage === totalPages ? "disabled" : ""}`}>
-                <button className="page-link" onClick={() => handlePageChange(currentPage + 1)}>Sau</button>
-              </li>
-            </ul>
-          </nav>
+           {/* Nút Xem thêm */}
+  <div className="text-center mt-4">
+    <a href="/blog" className="btn btn-outline-primary px-4 py-2">
+      Xem thêm
+    </a>
+  </div>
         </div>
       </section>
 
