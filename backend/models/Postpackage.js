5056abfce3e5a4131443e15dc6c605cc96cd55f8
tuanC <tuanthaiho89@gmail.com>
2025-07-22 00:26:46 +0700

    fix-dashboardadmin

--- backend/models/Postpackage.js
+++ backend/models/Postpackage.js
@@ -1,14 +1,15 @@
-import mongoose from 'mongoose';
-const { Schema } = mongoose; // ✅ Sửa lỗi này
+import mongoose from "mongoose";
+const { Schema, models, model } = mongoose;
 
 const postPackageSchema = new Schema({
-    type: {
-      type: String,
-      enum: ['VIP1', 'VIP2', 'VIP3', 'normal'],
-      required: true,
-    },
-    price: { type: Number },
-    expireAt: { type: Number },
-})
-const PostPackage = mongoose.model('PostPackage', postPackageSchema);
-export default PostPackage;
+  type: {
+    type: String,
+    enum: ["VIP1", "VIP2", "VIP3", "normal"],
+    required: true,
+  },
+  price: { type: Number },
+  expireAt: { type: Number },
+});
+
+// ✅ Kiểm tra nếu model đã tồn tại thì dùng lại
+export default models.PostPackage || model("PostPackage", postPackageSchema);
