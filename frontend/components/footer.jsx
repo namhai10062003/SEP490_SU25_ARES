0ec96283af55b313fa77c8f8c613e885ebce56c2
tuanC <tuanthaiho89@gmail.com>
2025-08-02 15:45:26 +0700

    fix-sth

--- frontend/components/footer.jsx
+++ frontend/components/footer.jsx
@@ -27,7 +27,7 @@ const Footer = () => {
                             <li><a href="/" className="text-light text-decoration-none">Trang chủ</a></li>
                             <li><a href="/blog" className="text-light text-decoration-none">Bài đăng nổi bật</a></li>
                             <li><a href="/gioi-thieu" className="text-light text-decoration-none">Về chúng tôi</a></li>
-                            <li><a href="#" className="text-light text-decoration-none">Liên hệ</a></li>
+                            <li><a href="/contact" className="text-light text-decoration-none">Liên hệ</a></li>
                         </ul>
                     </div>
 
